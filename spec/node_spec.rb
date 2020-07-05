06ff5972800914dd29fd4f2fcfacb43776867831
Zac <zjcrumpton@gmail.com>
2020-07-05 11:41:02 -0600

    change node property to next_node

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -11,21 +11,21 @@ describe Node do
       expect(node.contains).to eql("R")
     end
 
-    it "has a next property pointing to nil by default" do
+    it "has a next_node property pointing to nil by default" do
       node = Node.new
-      expect(node.next).to eql(nil)
+      expect(node.next_node).to eql(nil)
     end
 
-    it "takes an argument for pointing next at something" do
+    it "takes an argument for pointing next_node at something" do
       node = Node.new
-      node.next = Node.new("Y")
-      expect(node.next.contains).to eql("Y")
+      node.next_node = Node.new("Y")
+      expect(node.next_node.contains).to eql("Y")
     end
 
     it "acts as a linked list" do
       node = Node.new
-      node.next = Node.new("Y", Node.new("end"))
-      expect(node.next.next.contains).to eql("end")
+      node.next_node = Node.new("Y", Node.new("end"))
+      expect(node.next_node.next_node.contains).to eql("end")
     end
   end
 end
