19520fbee387e42e3bdd6ffb49f4033c5fd7c3c7
Zac <zjcrumpton@gmail.com>
2020-07-05 11:56:59 -0600

    define Node class with initialize method

---
+++ lib/node.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'pry'
+
+# Represents a single space on the Connect 4 Grid
+class Node
+  attr_accessor :contains, :next_node
+  def initialize(contains = nil, next_node = nil)
+    @contains = contains
+    @next_node = next_node
+  end
+end
