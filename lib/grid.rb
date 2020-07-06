a562d6c8b4ddf75442fec048d6c479eebabfee13
Zac <zjcrumpton@gmail.com>
2020-07-06 10:10:43 -0600

    add add_links method

--- lib/grid.rb
+++ lib/grid.rb
@@ -10,6 +10,7 @@ class Grid
 
   def initialize
     build_grid
+    add_links
   end
 
   def build_grid
@@ -19,14 +20,23 @@ class Grid
       Array.new(7) do
         column == 6 ? (row += 1; column = 0) : column += 1
         if row.zero?
-          Node.new(column)
+          Node.new(column, [row, column])
         elsif row == 7
-          Node.new('end')
+          Node.new('end', [row, column])
         else
-          Node.new
+          Node.new(nil, [row, column])
         end
       end
     end
   end
-end
 
+  def add_links
+    @board[0].each do |head|
+      current = head
+      until current.contains == 'end'
+        current.next_node = @board[current.location[0] + 1][current.location[1]]
+        current = current.next_node
+      end
+    end
+  end
+end
